03324e6ca75684601c6254c94e0731ec162dc233
Thyrak12 <thyrak9167@gmail.com>
2025-02-15 23:19:48 +0700

    header footer ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Header from './Header'
 import Footer from './Footer'
 
@@ -11,7 +7,6 @@ function App() {
     <>
       <Header />
       <Footer />
-      <p>Hi</p>
     </>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'; 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 export default function Header() {
   return (
