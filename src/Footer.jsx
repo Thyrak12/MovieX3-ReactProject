76f72cacdd710c076529b3c4812ddad4ec00517e
Povpanha28 <sengkim28140@gmail.com>
2025-02-15 22:48:25 +0700

    Add Header and Footer Component

---
+++ src/Footer.jsx
@@ -0,0 +1,35 @@
+import 'bootstrap/dist/css/bootstrap.css';
+export default function Footer() {
+    return (
+        <>
+            <footer class="bg-dark text-white py-4">
+                <div class="container text-center">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h5>MovieHub</h5>
+                            <p>&copy; 2025 MovieHub. All Rights Reserved.</p>
+                        </div>
+                        <div class="col-md-4">
+                            <h5>Quick Links</h5>
+                            <ul class="list-unstyled">
+                                <li><a href="#" class="text-white text-decoration-none">Home</a></li>
+                                <li><a href="#" class="text-white text-decoration-none">Movies</a></li>
+                                <li><a href="#" class="text-white text-decoration-none">TV Shows</a></li>
+                                <li><a href="#" class="text-white text-decoration-none">Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4">
+                            <h5>Follow Us</h5>
+                            <a href="#" class="text-white me-2"><i class="bi bi-facebook"></i></a>
+                            <a href="#" class="text-white me-2"><i class="bi bi-twitter"></i></a>
+                            <a href="#" class="text-white me-2"><i class="bi bi-instagram"></i></a>
+                            <a href="#" class="text-white"><i class="bi bi-youtube"></i></a>
+                        </div>
+                    </div>
+                    <hr class="bg-light" />
+                    <p class="mb-0">Privacy Policy | Terms of Service</p>
+                </div>
+            </footer>
+        </>
+    )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,42 @@
+import 'bootstrap/dist/css/bootstrap.css'; 
+
+export default function Header() {
+  return (
+    <>
+    <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container">
+        <a class="navbar-brand" href="#">MovieX3</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Movies</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link">Latest</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link">Favorite</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link">MyList</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </header>
+    </>
+  )
+}
