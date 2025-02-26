a8ef2fec4ef78546c3e547cafa600010569679d7
youdy <youdy017@gmail.com>
2025-02-26 15:49:50 +0700

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import Header from './Header'
 import Footer from './Footer'
 import Home from './Home'
+//import Movies from './Movies'
+//import Latest from './Latest'
+//import Favorite from './Favorite'
+//import MyList from './MyList'
+//import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
-
   return (
     <>
+      {/* <Router>
+        <div className='App'>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/Movies' component={Movies} />
+            <Route path='/Latest' component={Latest} />
+            <Route path='/Favorite' component={Favorite} />
+            <Route path='/Mylist' component={MyList} />
+          </Switch>
+        </div>
+      </Router> */}
       <Header />
       <Home />
       <Footer />
